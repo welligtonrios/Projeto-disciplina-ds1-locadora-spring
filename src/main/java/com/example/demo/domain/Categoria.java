7d03f9c9fb298d904a4c25b66d3d1fcbc40ae13b
Welligton Rios <wriosfaculdade@gmail.com>
2020-06-24 11:50:31 -0300

    Configurado banco H2 e persistencia com JPA

--- src/main/java/com/example/demo/domain/Categoria.java
+++ src/main/java/com/example/demo/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.example.demo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	/**
@@ -9,6 +15,8 @@ public class Categoria implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
