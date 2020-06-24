c2a2e6f7f87586075c02686d23649e5cb1e91cdf
Welligton Rios <wriosfaculdade@gmail.com>
2020-06-24 11:37:15 -0300

    criada primeira classe de dominio 'categoria'

--- src/main/java/com/example/demo/resouces/CategotiaResoursce.java
+++ src/main/java/com/example/demo/resouces/CategotiaResoursce.java
@@ -1,16 +1,28 @@
 package com.example.demo.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategotiaResoursce {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest Olá mundo";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Terror");
+		Categoria cat2 = new Categoria(2,"Comédia");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 	
